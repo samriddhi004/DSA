6f545a80372ffbb1ffd193fa3b4cb26b415d96ce
samriddhi004 <karkisamriddhi04@gmail.com>
2023-10-11 08:50:19 +0545

    linear search

---
+++ LinearSearch.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int LinearSearch(int *arr,int val){
+    for(int i =0;i<sizeof(arr);i++){
+        if(arr[i]==val){
+            return i;
+        }
+    }
+    return 0;
+}
+int main(){
+    int a[4]={1,2,3,4};
+    int val= 4;
+    cout<<"found at index: "<<LinearSearch(a,val);
+
+}
